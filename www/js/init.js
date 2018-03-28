b52ed518b48d338fabff51b402e8a56b256bd28e
Larussa <larussabrajko@gmail.com>
2018-03-28 17:59:09 +0300

    new changes

--- www/js/init.js
+++ www/js/init.js
@@ -1,9 +1,15 @@
   $( function() {
       //Init tabs
       $( "#tabs" ).tabs();
-  });
-
-  $( function() {
     //Init tabs
     $( "#portfolio-tabs" ).tabs();
-});
+ 
+// init slick carousel  
+    $('.reviews-carousel').slick({
+        dots:true,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa fa-angle-left"></i></div></div>',      
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-angle-right"></i></div></div>', 
+    });
+
+    });
+
